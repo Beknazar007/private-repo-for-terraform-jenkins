e182afcd2d79776bceef366dd7144354905576f6
Beknazar007 <saitovbeknazar6@gmail.com>
2023-09-05 04:58:33 +0600

    adding commits

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 provider "aws" {
   region = "us-east-1"  # Replace with your desired AWS region
 }
-
+terraform {
+  backend "s3" {    
+  }
+}
 resource "aws_instance" "example" {
   ami           = "ami-051f7e7f6c2f40dc1"
   instance_type = "t2.micro"
